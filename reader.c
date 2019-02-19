8845622dae15db8ab8cde3a5862022119033a6a4
Kaylin <kzaroukian@gmail.com>
2019-02-19 15:30:31 -0500

    reading and writing shared memory works

--- reader.c
+++ reader.c
@@ -1,23 +1,47 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <sys/types.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/ipc.h> 
+#include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#define MEM_SIZE 4096
 
 int main(){
-
+    int shmId;
+    char *shmPtr;
+    
+    struct shm_echo {
+        bool isWaiting[3];
+        char strToSend[500];
+    };
+    
 	//get key using ftok()
-	int key = ftok("fileloc", 1);
+	key_t key = ftok("writer.c", 1);
+    printf("Key: %d\n",key);
+
 	//
-	if ((shmPtr = shmat (key, 0, 0)) == (void*) -1) { 
+    if((shmId = shmget(key, MEM_SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+        perror("Error accessing shared memory");
+        exit(1);
+    }
+	if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
       		perror ("can't attach\n"); 
       		exit (1); 
    	}
 
 	//get struct pointer from shared memory
-	// struct = (struct*) shmPtr;
+    struct shm_echo *msgReceived = (struct shm_echo*) shmPtr;
+
 	while(1){
+        while (msgReceived->isWaiting[0]) {
+            printf("%s\n",msgReceived->strToSend);
+            msgReceived->isWaiting[0] = true;
+        }
+        msgReceived->isWaiting[0] = false;
 	
 		//while(struct.iswaiting[i] or exit);
 		//	if exit:
